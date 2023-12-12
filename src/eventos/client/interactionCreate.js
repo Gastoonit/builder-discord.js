552bcadcfc348ad2779a854d835809907dd8c2a1
Gastoon <87291111+Gastoonit@users.noreply.github.com>
2023-12-12 17:41:14 -0300

    Update interactionCreate.js

--- src/eventos/client/interactionCreate.js
+++ src/eventos/client/interactionCreate.js
@@ -1,19 +1,16 @@
-const { Events } = require('discord.js');
+const { Events } = require("discord.js");
 
 module.exports = {
   name: Events.InteractionCreate,
   emiter: "on",
   run: async (client, interaction) => {
-
-  if (interaction.isChatInputCommand()) {
-
-  const command = client.slashCommands.get(interaction.commandName);
-  try {
-  await command.run(client, interaction)
-  } catch (err) {
-   console.log(err)
-  }
-  }
-
- }
+    if (interaction.isChatInputCommand()) {
+      const command = client.slashCommands.get(interaction.commandName);
+      try {
+        await command.run(client, interaction);
+      } catch (err) {
+        console.log(err);
+      }
+    }
+  },
 };

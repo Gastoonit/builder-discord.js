f52062f5247a83d4b44861770e47bf4d0a277c16
Gastoon <87291111+Gastoonit@users.noreply.github.com>
2024-02-07 00:06:11 -0300

    Create embed.js

---
+++ src/slashCommands/generales/embed.js
@@ -0,0 +1,20 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("embed")
+    .setDescription("hola, descripción!"),
+    run: async (client, interaction) => {
+	    
+     const Embed = new EmbedBuilder()
+      .setTitle("Título del embed")
+      .setDescription("Descripción del embed")
+      .setColor("Random")
+
+     return interaction.reply({
+	       content: "- 🙋 | hola, soy un bot alegre! // hi i'm a happy bot",
+	       embeds: [Embed],
+     });
+
+ }
+}
